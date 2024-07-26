a64be61cc3c2dbb385938d835706aa703af90d69
lex232 <alexeikrivonosov@mail.ru>
2024-07-26 19:04:16 +0300

    redesign main page

--- frontend/html/assets/js/support-ticket-custom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var support_ticket_datatable = {
-    init: function() {
-    	$('#basic-6').DataTable();
-    }
-};
-$(document).ready(function() {
-    support_ticket_datatable.init()
-});
